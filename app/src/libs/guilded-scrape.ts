8c53594fa2d501672b9cda30b7b71822d283467e
Soham Jaiswal <sosweetham@gmail.com>
2024-05-05 10:06:14 +0530

    fix: remove console logs

--- app/src/libs/guilded-scrape.ts
+++ app/src/libs/guilded-scrape.ts
@@ -4,35 +4,24 @@ import puppeteer, { Browser } from "puppeteer";
 export let browserInstances: Browser[] = []
 
 export const guildedUserProfileScrape = async (id: string, getElement: 'avatar' | 'banner') => {
-    console.log('Reached Scraper')
     const getClass = getElement === 'avatar' ? '.UserProfilePictureControl-picture' : '.UserProfileBackground-image'
-    console.log('launching puppeteer')
     const browser = await puppeteer.launch({ 
         headless: true,
         args: ['--no-sandbox', '--disable-setuid-sandbox'],
         executablePath: "/usr/bin/chromium",
     })
-    console.log('puppeteer launched')
     browserInstances.push(browser)
-    console.log('opening new page')
     const page = await browser.newPage()
-    console.log('opened new page, going to url')
     await page.goto(`https://guilded.gg/profile/${id}`, {waitUntil: 'networkidle0'})
-    console.log('went to url')
     const src = await page.$eval(getClass, (el: any) => el.src)
-    console.log('got src')
     if (getElement === 'avatar') {
         await userAvatarBucket.uploadImage(id, src)
     } else if (getElement === 'banner') {
         await userBannerBucket.uploadImage(id, src)
     }
-    console.log('uploaded image')
     await page.close()
-    console.log('closed page')
     await browser.close()
-    console.log('closed browser')
     browserInstances.pop()
-    console.log('popped browser, returning fetched image')
     return await (await fetch(src)).blob()
 }
 
